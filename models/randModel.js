1b4676e6edeafcf5f5065292dbb557b4970553ff
Ian Hart <ianhart13@gmail.com>
2023-01-06 15:21:56 -0500

    initial commit setup base express & mongoDB connection

---
+++ models/randModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const SomeModelSchema = new Schema({
+  a_string: String,
+  a_date: Date
+});
+
+const SomeModel = mongoose.model('SomeModel', SomeModelSchema);
